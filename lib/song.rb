4393258f46b79d8e0cb114df6be9ba2e01af0a1b
Gustavo Villagrana <gusvilla303@gmail.com>
2017-09-20 00:48:13 +0000

    Add Concerns::Findable module

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 class Song
 
+  extend Concerns::Findable
+
   attr_accessor :name
   attr_reader :artist, :genre
 
@@ -42,17 +44,4 @@ class Song
       genre.songs << self
     end
   end
-
-  def self.find_by_name(name)
-    @@all.detect { |song| song.name == name }
-  end
-
-  def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      self.find_by_name(name)
-    else
-      self.create(name)
-    end
-  end
-
 end
